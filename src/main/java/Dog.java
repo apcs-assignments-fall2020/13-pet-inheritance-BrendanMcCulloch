2d0e2bae5a969b23e1a111d395e050122eb90e90
BrendanMcCulloch <22bmcculloch@athenian.org>
2020-11-02 03:33:55 +0000

    created class

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,41 @@
 public class Dog extends Pet {
-    // Instance variable(s)
+    private String breed;
+
+    public Dog() {
+        super();
+        this.breed = "unknown";
+    }
+
+        public Dog(String Name, int age, String breed){
+            super(name, age);
+                this.breed = breed;
+        }
 
 
     // Constructors
     
 
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Bark!");
+    }
 
     // toString method
+    public String toString() {
+        String dogthings = "Name: " + this.getName() + ", Age: " + this.getAge() + ", breed: " + breed;
+        return dogthings;
+    }
 
 
     // Getter
-
+    public String getbreed() {
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed) {
+        if(breed.trim().length() != 0){
+            this.breed = breed;
+        }
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,7 @@
 public class DogClient {
     public static void main(String[] args) {
+        Dog check = new Dog();
+        System.out.println(check);
         
     }
 }
